56dc63ec7793dcbfb29169444ff5e833270612e5
SaintMiner <dinden290@gmail.com>
2020-12-24 23:06:52 +0200

    top added

--- src/commands/Top.js
+++ src/commands/Top.js
@@ -1,18 +1,15 @@
 const Command = require('../classes/command.js');
+const { MessageEmbed } = require('discord.js');
 
 class TopCommand extends Command {
 
     constructor(client) {
         super(client, {
-            slug: 'useless', //how command can be executed
+            slug: 'top', //how command can be executed
             permissions: [], //discord server permissions
             systemAdmin: false, //only system administrators can launch this command
         });
     }
 
-    executeCustom(message, args) {
-        message.channel.send('The useless command');
-    }
-}
-
-module.exports = TopCommand;
+    async executeCustom(message, args) {
+        let voice_profiles = this.client.st
