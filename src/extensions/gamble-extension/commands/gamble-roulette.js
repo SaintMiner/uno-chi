c86cc3977b639665bf2b7b054c70fd00009c221a
SaintMiner <dinden290@gmail.com>
2021-02-14 23:02:20 +0200

    extension integration and updating

---
+++ src/extensions/gamble-extension/commands/gamble-roulette.js
@@ -0,0 +1,51 @@
+
+function startRoulette(message, args) {
+    let gambleExtension = core.getExtension('GambleExtension');
+    let customArgs = message.content
+        .toLowerCase()
+        .slice(core.configuration.prefix.length)
+        .trim()
+        .split(/ +/g)
+        .filter(part => !!part);
+        
+    gambleExtension.roulette(message, customArgs.slice(1), message.author.id, message.guild.id);
+}
+
+let roulette = {
+    slug: 'roulette',
+    description: null,
+    category: null,
+
+    usages: [],
+    aliases: [],
+    permissions: [],
+    whiteListedUsers: [],
+
+    isHidden: false,
+    isPrivate: false,
+
+    execute: startRoulette,
+    childrens: [],
+}
+
+let gamble = {
+    slug: 'gamble',
+    description: null,
+    category: null,
+
+    usages: [],
+    aliases: [],
+    permissions: [],
+    whiteListedUsers: [],
+
+    isHidden: false,
+    isPrivate: false,
+
+    execute: null,
+    childrens: [
+        roulette
+    ],
+}
+
+module.exports = gamble;
+
