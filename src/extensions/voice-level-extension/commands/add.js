7b73c37a59a5182b7d8c4c0e533cc2693dfbf039
SaintMiner <dinden290@gmail.com>
2021-09-11 20:09:51 +0300

    add to transactions

--- src/extensions/voice-level-extension/commands/add.js
+++ src/extensions/voice-level-extension/commands/add.js
@@ -1,33 +1,47 @@
-function addVoicePoints(message, args, overage) {
-    const voiceProfileExtension = core.getExtension('VoiceProfileExtension');
+const { show, update, transaction } = require('../rest.js');
+
+async function addVoicePoints(message, args, overage) {
+    // const voiceProfileExtension = core.getExtension('VoiceProfileExtension');
     let who = message.mentions.members.first();
     let voicePointsAmount = overage[1];
 
     if (!who) return core.sendLocalizedError(message, `WHO_MUST_BE_SPECIFIED`);
     if (isNaN(voicePointsAmount)) return core.sendLocalizedError(message, `ADD_VOICEPOINTS_AMOUNT_MUST_BE_SPECIFIED`);
     
-    let profile = voiceProfileExtension.findVoiceProfile(who.id, message.guild.id);
+    // let profile = voiceProfileExtension.findVoiceProfile(who.id, message.guild.id);
+    let profile = await show(message.guild.id, who.id);
 
     if (!profile) return core.sendLocalizedError(message, `VOICE_PROFILE_NOT_FOUND`);
 
-    profile.voicepoints += Math.round(voicePointsAmount);
+    // profile.voicepoints += Math.round(voicePointsAmount);
+    await transaction({
+        from: "self",
+        to: {
+            user_id: profile.user_id,
+            guild_id: profile.guild_id,
+        },
+        amount: Math.round(voicePointsAmount),
+        reason: "Voice add voicepoints",
+    });
 
     core.sendSuccessful(message);
 }
 
-function addVoiceXp(message, args, overage) {
-    const voiceProfileExtension = core.getExtension('VoiceProfileExtension');
+async function addVoiceXp(message, args, overage) {
+    // const voiceProfileExtension = core.getExtension('VoiceProfileExtension');
     let who = message.mentions.members.first();
     let xpAmount = overage[1];
 
     if (!who) return core.sendLocalizedError(message, `WHO_MUST_BE_SPECIFIED`);
     if (isNaN(xpAmount)) return core.sendLocalizedError(message, `ADD_EXPERIENCE_AMOUNT_MUST_BE_SPECIFIED`);
 
-    let profile = voiceProfileExtension.findVoiceProfile(who.id, message.guild.id);
+    // let profile = voiceProfileExtension.findVoiceProfile(who.id, message.guild.id);
+    let profile = await show(message.guild.id, who.id);
 
     if (!profile) return core.sendLocalizedError(message, `VOICE_PROFILE_NOT_FOUND`);
 
     profile.experience += Math.round(xpAmount);
+    update(profile);
 
     core.sendSuccessful(message);
 }
@@ -51,8 +65,8 @@ let voicexp = {
 let command = {
     permissions: ['ADMINISTRATOR'],
     slug: 'add',
-    execute: stopper
-    // childrens: [ voicexp, voicepoints ],
+    // execute: stopper
+    childrens: [ voicexp, voicepoints ],
 }
 
 module.exports = command;
